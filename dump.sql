3f36e4e3946d894c373e8203e913ddc31b4412a7
Aldo Vandus <aldovandus@gmail.com>
2018-08-29 18:55:22 +0200

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `course_management` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `course_management`;
 -- MySQL dump 10.16  Distrib 10.1.26-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: 127.0.0.1    Database: course_management
@@ -10,9 +12,31 @@
 /*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `indirizzo` varchar(30) DEFAULT NULL,
+  `civico` varchar(10) DEFAULT NULL,
+  `cap` char(5) DEFAULT NULL,
+  `citta` varchar(30) DEFAULT NULL,
+  `p_iva` varchar(30) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `note` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `cliente`
@@ -25,6 +49,32 @@ INSERT INTO `cliente` VALUES (1,'Via 1','123','1234','Roma','0123456789','123456
 UNLOCK TABLES;
 
 --
+-- Table structure for table `corso`
+--
+
+DROP TABLE IF EXISTS `corso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `corso` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `codice` varchar(5) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `ore` int(11) NOT NULL,
+  `data_inizio` date DEFAULT NULL,
+  `inizio_stage` date DEFAULT NULL,
+  `luogo_stage` varchar(30) DEFAULT NULL,
+  `data_fine` date DEFAULT NULL,
+  `data_termine10` date DEFAULT NULL,
+  `aula` varchar(45) DEFAULT NULL,
+  `ora_inizio` int(11) DEFAULT NULL,
+  `ora_fine` int(11) DEFAULT NULL,
+  `data_esame` date DEFAULT NULL,
+  `note` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `corso`
 --
 
@@ -35,6 +85,23 @@ INSERT INTO `corso` VALUES (1,'1','OSS',1000,'0000-00-00','0000-00-00','','0000-
 UNLOCK TABLES;
 
 --
+-- Table structure for table `dvr_strumentazione`
+--
+
+DROP TABLE IF EXISTS `dvr_strumentazione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dvr_strumentazione` (
+  `id_societa` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `data_strumento` date DEFAULT NULL,
+  `formazione` varchar(45) DEFAULT NULL,
+  `data_formazione` date DEFAULT NULL,
+  PRIMARY KEY (`id_societa`,`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `dvr_strumentazione`
 --
 
@@ -42,6 +109,31 @@ LOCK TABLES `dvr_strumentazione` WRITE;
 /*!40000 ALTER TABLE `dvr_strumentazione` DISABLE KEYS */;
 /*!40000 ALTER TABLE `dvr_strumentazione` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `fattura`
+--
+
+DROP TABLE IF EXISTS `fattura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fattura` (
+  `numero` int(11) NOT NULL,
+  `anno` char(4) NOT NULL,
+  `data` date DEFAULT NULL,
+  `prestazione` longtext,
+  `importo` decimal(10,3) DEFAULT NULL,
+  `iva` decimal(10,3) DEFAULT '0.000',
+  `con_cassa` decimal(10,3) DEFAULT '0.000',
+  `ritenuta` decimal(10,3) DEFAULT '0.000',
+  `bolli` decimal(10,3) DEFAULT '0.000',
+  `totale` decimal(10,3) DEFAULT '0.000',
+  `id_cliente` int(11) DEFAULT NULL,
+  PRIMARY KEY (`numero`,`anno`),
+  KEY `fk_fattura_cliente_idx` (`id_cliente`),
+  CONSTRAINT `fk_fattura_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `fattura`
@@ -54,6 +146,27 @@ INSERT INTO `fattura` VALUES (1,'2017','2017-01-11','Corso OSS',1000.000,100.000
 UNLOCK TABLES;
 
 --
+-- Table structure for table `iscrizione`
+--
+
+DROP TABLE IF EXISTS `iscrizione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `iscrizione` (
+  `codice` int(11) NOT NULL AUTO_INCREMENT,
+  `idcliente` int(11) NOT NULL,
+  `idcorso` int(11) NOT NULL,
+  `prezzo` decimal(10,0) DEFAULT NULL,
+  `esito_esame` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`codice`),
+  KEY `iscrizione_cliente_fk_idx` (`idcliente`),
+  KEY `iscrizione_corso_fk_idx` (`idcorso`),
+  CONSTRAINT `iscrizione_cliente_fk` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `iscrizione_corso_fk` FOREIGN KEY (`idcorso`) REFERENCES `corso` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1275 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `iscrizione`
 --
 
@@ -62,6 +175,25 @@ LOCK TABLES `iscrizione` WRITE;
 INSERT INTO `iscrizione` VALUES (1,1,1,1000,''),(2,1,2,800,NULL),(3,2,1,1000,NULL),(4,2,2,700,NULL);
 /*!40000 ALTER TABLE `iscrizione` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `privato`
+--
+
+DROP TABLE IF EXISTS `privato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `privato` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(30) DEFAULT NULL,
+  `cognome` varchar(30) DEFAULT NULL,
+  `data_nascita` date DEFAULT NULL,
+  `cf` char(16) DEFAULT NULL,
+  UNIQUE KEY `CF_UNIQUE` (`cf`),
+  KEY `fk_privato_idx` (`id`),
+  CONSTRAINT `fk_privato` FOREIGN KEY (`id`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `privato`
@@ -74,6 +206,24 @@ INSERT INTO `privato` VALUES (1,'Mario','Rossi','1968-11-10','123456789'),(2,'Pi
 UNLOCK TABLES;
 
 --
+-- Table structure for table `rata`
+--
+
+DROP TABLE IF EXISTS `rata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rata` (
+  `cod_iscrizione` int(11) NOT NULL,
+  `num_rata` int(11) NOT NULL,
+  `importo` decimal(10,2) DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  `metodo` varchar(45) DEFAULT NULL,
+  `num_ricevuta` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`cod_iscrizione`,`num_rata`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `rata`
 --
 
@@ -82,6 +232,23 @@ LOCK TABLES `rata` WRITE;
 INSERT INTO `rata` VALUES (1,1,100.00,'2017-02-14','Contanti','0001'),(1,2,200.00,'2017-03-17','Contanti','0002');
 /*!40000 ALTER TABLE `rata` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `societa`
+--
+
+DROP TABLE IF EXISTS `societa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `societa` (
+  `id` int(11) NOT NULL,
+  `rag_sociale` varchar(150) DEFAULT NULL,
+  `rag_sociale2` varchar(150) DEFAULT NULL,
+  `dvr` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_societa_cliente` FOREIGN KEY (`id`) REFERENCES `cliente` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `societa`
@@ -95,9 +262,10 @@ UNLOCK TABLES;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-29 17:57:47
+-- Dump completed on 2018-08-29 18:50:38
