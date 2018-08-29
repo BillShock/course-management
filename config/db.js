925f68c667160f112ac4d1f7ebd5f61a5210a615
amal <amal@debian>
2018-08-29 18:06:44 +0200

    db file config updated

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 
 const Sequelize = require('sequelize');
 
-  const sequelize = new Sequelize('fatturazione', 'root', '', {
+  const sequelize = new Sequelize('course_management', 'root', '', {
     host: '127.0.0.1',
     dialect: 'mysql',
     port: 3308,
